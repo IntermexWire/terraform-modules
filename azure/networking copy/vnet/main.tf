2fcd90808575c426712abfca5a2b97cd5dd97ea4
vsem-imxi <vsem@intermexusa.com>
2024-06-16 20:44:49 -0400

    remove "networking copy" folder

--- azure/networking copy/vnet/main.tf
+++
@@ -1,9 +0,0 @@
-# Create VNets conditionally based on the 'create' flag
-resource "azurerm_virtual_network" "vnet" {
-  for_each = { for k, v in var.vnets : k => v if v.create }
-
-  name                = each.value.name
-  address_space       = each.value.address_space
-  location            = each.value.location
-  resource_group_name = var.resource_group_name
-}

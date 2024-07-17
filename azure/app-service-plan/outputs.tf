757484132c60f6b5f91c9c69707421886281c30b
vsem-imxi <vsem@intermexusa.com>
2024-07-17 06:51:15 -0400

    Add app servics and storage modules

--- azure/app-service-plan/outputs.tf
+++ azure/app-service-plan/outputs.tf
@@ -1,14 +1,11 @@
-output "names" {
-  description = "The names of the service plans"
-  value       = { for k, v in azurerm_service_plan.service_plan : k => v.name }
+output "id" {
+  value = azurerm_app_service_plan.example.id
 }
 
-output "ids" {
-  description = "The ids of the service plans"
-  value       = { for k, v in azurerm_service_plan.service_plan : k => v.id }
+output "kind" {
+  value = azurerm_app_service_plan.example.kind
 }
 
-output "kinds" {
-  description = "The kinds of the service plans"
-  value       = { for k, v in azurerm_service_plan.service_plan : k => v.kind }
+output "name" {
+  value = azurerm_app_service_plan.example.name
 }
--- azure/storage-account/main.tf
+++ azure/storage-account/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_storage_account" "sa" {
-  for_each                 = var.create ? toset(var.locations) : []
-  name                     = "${var.name}${each.key}"
-  location                 = each.key
-  resource_group_name      = var.resource_group_name
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  min_tls_version          = var.min_tls_version
+  for_each                          = var.create ? toset(var.locations) : []
+  name                              = "${var.name}${each.key}"
+  location                          = each.key
+  resource_group_name               = var.resource_group_name
+  account_tier                      = var.account_tier
+  account_replication_type          = var.account_replication_type
+  min_tls_version                   = var.min_tls_version
   infrastructure_encryption_enabled = var.infrastructure_encryption_enabled
 
   network_rules {

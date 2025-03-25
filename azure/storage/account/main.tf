c8ede26728c81de9ee06c04923e2e35f5124a9ff
vsem-imxi <vsem@intermexusa.com>
2025-03-25 17:18:37 -0400

    snet, container app env, and storage account refactor

--- azure/storage/account/main.tf
+++ azure/storage/account/main.tf
@@ -3,11 +3,38 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  account_tier             = var.tier
-  account_replication_type = var.replication_type
+  account_kind             = var.account_kind
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
   access_tier              = var.access_tier
 
-  infrastructure_encryption_enabled = var.infrastructure_encryption
+  infrastructure_encryption_enabled = var.infrastructure_encryption_enabled
+  public_network_access_enabled     = var.public_network_access_enabled
+  https_traffic_only_enabled        = var.https_traffic_only_enabled
+  shared_access_key_enabled         = var.shared_access_key_enabled
+  min_tls_version                   = var.min_tls_version
+
+  is_hns_enabled           = var.is_hns_enabled
+  large_file_share_enabled = var.large_file_share_enabled
+
+  nfsv3_enabled = var.nfsv3_enabled
+  sftp_enabled  = var.sftp_enabled
+
+  dynamic "custom_domain" {
+    for_each = var.custom_domain != null ? [var.custom_domain] : []
+    content {
+      name          = custom_domain.value.name
+      use_subdomain = custom_domain.value.use_subdomain
+    }
+  }
+
+  dynamic "static_website" {
+    for_each = var.static_website != null ? [var.static_website] : []
+    content {
+      index_document     = static_website.value.index_document
+      error_404_document = static_website.value.error_404_document
+    }
+  }
 
   network_rules {
     default_action             = var.network_rules.default_action
